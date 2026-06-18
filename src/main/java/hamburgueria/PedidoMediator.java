946d301355ec2c2b1dff8ed5510b05434b3f6b8c
Rafael Toledo Souza <ch1at@outlook.com>
2026-06-17 23:22:11 -0300

    Integração fila de pedidos e relatório de caixa

--- src/main/java/hamburgueria/PedidoMediator.java
+++ src/main/java/hamburgueria/PedidoMediator.java
@@ -1,11 +1,16 @@
 package hamburgueria;
 
+import hamburgueria.Pedidos.FilaDePedidos;
+import hamburgueria.Pedidos.RegistroDePedido;
 import hamburgueria.Setores.Caixa;
 import hamburgueria.Setores.Cozinha;
 
 public class PedidoMediator {
 
     private static PedidoMediator instancia = new PedidoMediator();
+
+    private final FilaDePedidos historicoGlobal = new FilaDePedidos();
+    private int sequencialPedidos = 1;
 
     private PedidoMediator() {}
 
@@ -26,9 +31,15 @@ public class PedidoMediator {
     }
 
     public String receberPagamentoAtendente(Combo combo) {
+        RegistroDePedido registro = new RegistroDePedido(sequencialPedidos++, combo);
+        registro.marcarComoEntregue();
+        historicoGlobal.adicionarPedido(registro);
+
         return "O PedidoMediator registrou o pagamento e notificou os setores.\n" +
                 ">>" + new Caixa().receberPagamento(combo) + "\n" +
-                ">>" + new Cozinha().receberPagamento(combo);
+                ">>" + new Cozinha().receberPagamento(combo) + "\n" +
+                "[Métrica Sistema] Total de receita acumulada no Caixa: R$ " +
+                String.format("%.2f", RelatorioDeCaixa.calcularReceitaTotal(historicoGlobal));
     }
 
 }
