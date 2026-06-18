84fd752f73d54722de3188f9de99b9cb88f5cb42
Rafael Toledo Souza <ch1at@outlook.com>
2026-06-17 23:44:04 -0300

    Centralizando Cardapio e Promocao na HamburgueriaPedidoFacade

--- src/main/java/hamburgueria/HamburgueriaPedidoFacade.java
+++ src/main/java/hamburgueria/HamburgueriaPedidoFacade.java
@@ -1,13 +1,28 @@
 package hamburgueria;
 
+import hamburgueria.Cardapio.Cardapio;
+import hamburgueria.Cardapio.ItemHamburguer;
+import hamburgueria.Cardapio.SecaoCardapio;
 import hamburgueria.Combos.FabricaComboTradicional;
 import hamburgueria.Combos.FabricaComboVegano;
+import hamburgueria.Descontos.DescontoPromocaoPrototype;
 import hamburgueria.Setores.Caixa;
 import hamburgueria.Setores.Cozinha;
 
 public class HamburgueriaPedidoFacade {
 
     private final Atendente atendente = new Atendente();
+    private final Cardapio cardapioDoSistema;
+    private final Promocao promocaoCadastrada;
+
+    public HamburgueriaPedidoFacade(Cardapio cardapioDoSistema, Promocao promocaoCadastrada) {
+        this.cardapioDoSistema = cardapioDoSistema;
+        this.promocaoCadastrada = promocaoCadastrada;
+    }
+
+    public String exibirCardapioSincronizado() {
+        return cardapioDoSistema.getCardapio();
+    }
 
     public Combo criarPedidoTradicional() {
         FabricaDeCombo fabrica = new FabricaComboTradicional();
@@ -31,22 +46,17 @@ public class HamburgueriaPedidoFacade {
                 .build();
     }
 
-    public Combo criarPedidoPersonalizado(FabricaDeCombo fabrica) {
-        return new ComboBuilder()
-                .setHamburguer(fabrica.criarHamburguer())
-                .setAcompanhamento(fabrica.criarAcompanhamento())
-                .setBebida(fabrica.criarBebida())
-                .adicionarObserver(new Cozinha())
-                .adicionarObserver(new Caixa())
-                .build();
-    }
-
     public String enviarPedido(Combo combo) {
         return atendente.fazerPedido(combo);
     }
 
-    public String aplicarDescontoEProcessarPagamento(Combo combo,
-                                                     EstrategiaDeDesconto desconto) {
+    public String aplicarDescontoPromocionalPrototype(Combo combo) {
+        EstrategiaDeDesconto descontoPrototype = new DescontoPromocaoPrototype(promocaoCadastrada);
+        combo.setEstrategiaDeDesconto(descontoPrototype);
+        return atendente.processarPagamento(combo);
+    }
+
+    public String aplicarDescontoEProcessarPagamento(Combo combo, EstrategiaDeDesconto desconto) {
         combo.setEstrategiaDeDesconto(desconto);
         return atendente.processarPagamento(combo);
     }
